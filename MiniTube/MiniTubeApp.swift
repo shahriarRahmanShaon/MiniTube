580d00ef3e02ebeab96ca006df8be445b3c137f9
shahriarRahmanShaon <shaon463@gmail.com>
2022-02-18 19:04:11 +0600

    Initial Commit

---
+++ MiniTube/MiniTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniTubeApp.swift
+//  MiniTube
+//
+//  Created by sergio shaon on 18/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MiniTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
